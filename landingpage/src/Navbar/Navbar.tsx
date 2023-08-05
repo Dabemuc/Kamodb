97d7aadc79f037cdc149c8945f2576e29752f0a2
Daniel Benner (ext.) <Daniel.Benner@consol.de>
2023-08-05 14:56:56 +0200

    adapted navbar

--- landingpage/src/Navbar/Navbar.tsx
+++ landingpage/src/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Navbar.css'
-import Logo from '../Kamodb-Logo.png'
 import LoginWrapper from '../LoginWrapper/LoginWrapper'
 
 interface Props {
@@ -11,7 +10,7 @@ interface Props {
 export default function Navbar(props: Props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
-        <a className="navbar-brand" href="#"><img src={Logo} alt="" width="100rem" height="25rem" /></a>
+        <a className="navbar-brand" href="#"><img alt="Kamodb-Logo-Alt" width="100rem" height="25rem" /></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
